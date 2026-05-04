758c47f1a577e046aa57853da1c3a13064bc4823
devmork <enoymarkden@gmail.com>
2026-05-04 18:59:07 +0800

    Refactor routes to use Inertia for home, login, and registration pages

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'message' => 'Hello, Inertia!',
-    ]);
-});
-
-
-Route::get('/about', function () {
-    return Inertia::render('About', [
-        'message' => 'Hello, Inertia!',
-    ]);
-});
+Route::inertia('/', 'home')->name('home');
+Route::inertia('/login', 'Auth/Login')->name('login');
+Route::inertia('/register', 'Auth/Register')->name('register');
