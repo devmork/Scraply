9558bef97bd2195b38e816b144f3a7e755910e4d
PaulandrewIgloso <iglosopaul310@gmail.com>
2026-05-16 21:32:52 +0800

    feat: add user profile route to protected routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
 // Protected Routes
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', fn() => Inertia::render('AuthenticatedLayout'))->name('dashboard');
+    Route::inertia('/profile', 'UserProfile/Profile')->name('profile');
 });
 
 Route::inertia('/listings/create', 'Listings/Create')->name('listings.create');
