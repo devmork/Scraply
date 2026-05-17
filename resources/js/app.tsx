4287c532e16875a73badcc5ed00cbeb11a5d26cb
janjanFlores <jhonlloydlawagonflores21@gmail.com>
2026-05-17 23:07:44 +0800

    fix: add type parameter to import.meta.glob for better type inference

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -7,7 +7,7 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob<true, './pages/**/*.tsx'[]>('./pages/**/*.tsx')),
   setup({ el, App, props } : any) {
     const root = createRoot(el)
     root.render(<App {...props} />)
