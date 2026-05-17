454bbb7db892dd811d430e46f0a2a1294bce4ec3
janjanFlores <jhonlloydlawagonflores21@gmail.com>
2026-05-17 23:07:33 +0800

    feat: add Junk model with fillable attributes and type casting

---
+++ app/Models/Junk.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Junk extends Model
+{
+    protected $fillable = [
+        'title',
+        'quantity',
+        'unit',
+        'price',
+        'isFree',
+        'pickupDate',
+        'pickupTime',
+        'pickupAvailability',
+        'pickupLocation',
+        'latitude',
+        'longitude',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'isFree' => 'boolean',
+        'pickupDate' => 'date',
+        'latitude' => 'float',
+        'longitude' => 'float',
+    ];
+}
