a483fa207f986c08dfabe38b21b5eec57df3b7cc
devmork <enoymarkden@gmail.com>
2026-05-17 12:55:26 +0800

    feat: wrap AuthenticatedLayout in SidebarProvider for improved context management

--- resources/js/pages/AuthenticatedLayout.tsx
+++ resources/js/pages/AuthenticatedLayout.tsx
@@ -1,21 +1,22 @@
 // resources/js/pages/AuthenticatedLayout.tsx
 import React from "react";
 import AppSidebar from "@/components/layout/Sidebar";
-import TopHeader from "@/components/layout/TopHeader";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen bg-gray-50">
-      {/* Sidebar */}
-      <AppSidebar />
+    <SidebarProvider>
+      <div className="flex h-screen bg-gray-50">
+        {/* Sidebar */}
+        <AppSidebar />
 
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col">
-        <TopHeader />
-        <main className="flex-1 overflow-auto">
-          {children}
-        </main>
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col">
+          <main className="flex-1 overflow-auto">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
